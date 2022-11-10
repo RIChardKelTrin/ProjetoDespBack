f1f812e992c9ea2d5c75693909717280e72ad94b
Lucas Kenzo <kenzoawane@gmail.com>
2022-11-10 00:49:59 -0300

    add backend

---
+++ Despachantes/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Despachantes.Model
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        [MinLength(11), MaxLength(11), Required(ErrorMessage = "O Cpf é obrigatório")]
+        public string Cpf { get; set; }
+    }
+}
